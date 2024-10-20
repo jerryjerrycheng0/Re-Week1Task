55996c766ac38886007647d50b95a404cb85788e
jerryjerrycheng0 <jerryjerrycheng0@gmail.com>
2024-10-20 17:48:34 +0100

    Added Gitignore

---
+++ Game Dev 3 - Week1/Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    //By making the bool static and public, we can access the variable from anywhere without referencing
+    //Be careful with using static, since it makes sense when there is only one istance of this object in the whole project!
+    //For example, do not use static for the enemyHp variable, since there would be more than on!
+    public static bool isGameOn = false;
+
+    public UnityEvent gameStarted;
+
+
+    public void GameIsOn()
+    {
+        isGameOn = true;
+
+        gameStarted.Invoke();
+    }
+
+    public void GameIsOff()
+    {
+        isGameOn = false;
+    }
+}
